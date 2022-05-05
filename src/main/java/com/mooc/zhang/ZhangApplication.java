af6427ebc511219b08fc5ae7275eeee2677b25b5
Jeremy <15230116652@163.com>
2022-05-05 22:30:41 +0800

    2-2 init

---
+++ src/main/java/com/mooc/zhang/ZhangApplication.java
@@ -0,0 +1,13 @@
+package com.mooc.zhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mooc/zhang/ZhangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mooc.zhang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
